d28b63c1f8b13cd8f1bed69d9085c887e03dc188
lukichova15 <kateryna.lukichova.ik.2023@lpnu.ua>
2023-09-27 23:05:01 +0300

    first master commit

---
+++ Lab_02/Lab_02.cpp
@@ -0,0 +1,25 @@
+/*Lab_02.cpp
+Лукічова Катерина
+Лабораторна робота 2
+Лінійні програми
+Варіанти 12*/
+
+#include <iostream>
+#include <cmath> // Додайте цей рядок
+using namespace std;
+
+int main() {
+	const double pi = 3.141592653589793238; // Використовуйте M_PI замість власної константи
+	double a; // вхідний параметр 
+	double z1, z2; // результати обчислення 1-го та 2-го  виразу відповідно
+	cout << "a = "; cin >> a;
+
+	z1 = (sin(4 * a) / (1 + (cos(4 * a)))) * (cos(2 * a) / (1 + (cos(2 * a))));
+	z2 = 1 / tan(3 / 2 * pi - a);
+
+	cout << endl;
+	cout << "z1 =" << z1 << endl;
+	cout << "z2 =" << z2 << endl;
+	cin.get();
+	return 0;
+}
